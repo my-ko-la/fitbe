39bc97c70e9150ee0b8ba13b0e6671514013a55f
Mykola Brechko <m.brechko@omisoft.net>
2023-10-22 15:16:37 +0200

    Axum basics

---
+++ back/src/main.rs
@@ -0,0 +1,52 @@
+#![allow(unused)]
+
+use std::net::SocketAddr;
+use serde::Deserialize;
+use axum::{Router, routing::{get, get_service}, response::{Html, IntoResponse}, extract::{Query, Path}};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
+
+
+    let addr = SocketAddr::from(([127,0,0,1], 8080));
+    println!("->LISTENING ON {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    
+    println!("{:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Hello, <strong>{name}!</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    
+    println!("{:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello, <strong>{name}!</strong>"))
+}
